36fe9df1e1ae1ebf6b03334bafe68aa5a0c253b5
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-11-10 22:02:50 +0100

    Added signature

--- sign.py
+++ sign.py
@@ -1,12 +1,36 @@
 #!/usr/local/env python
 
 import sys
+import base64
+import time
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+
+def private_key_encrypt(message, private_key):
+    cipher = PKCS1_OAEP.new(private_key)
+    encrypted_msg = cipher.encrypt(bytes(message, 'utf-8'))
+    enc_text = base64.b64encode(encrypted_msg)
+    return enc_text
+
+
+def sign_license(email, expiry, private_key):
+    message = f'{email}:{expiry}'
+    return private_key_encrypt(message, private_key)
+
+
+def read_public_key(filename):
+    with open(filename, "rb") as f:
+        return RSA.importKey(f.read())
+    
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python verify.py <public_key_file>")
+    if len(sys.argv) < 3:
+        print("Usage: python sign.py <private_key_file> <email> <expiry>")
         sys.exit(1)
-    with open(sys.argv[1], "rb") as f:
-        key = RSA.importKey(f.read())
-        print(key)
+    
+    key = read_public_key(sys.argv[1])
+    email = sys.argv[2]
+    expiry, = sys.argv[3:4] or [ str(int(time.time())) ]
+    
+    print(sign_license(email, expiry, key))
