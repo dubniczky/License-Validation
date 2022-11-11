4235f2ab1ebe140846e4ab0d7b12394df8c18d89
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-11-12 00:54:41 +0100

    Updated signature generation expiry

--- sign.py
+++ sign.py
@@ -1,22 +1,21 @@
 #!/usr/local/env python
 
 import sys
-import base64
 import time
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-
-
-def private_key_encrypt(message, private_key):
-    cipher = PKCS1_OAEP.new(private_key)
-    encrypted_msg = cipher.encrypt(bytes(message, 'utf-8'))
-    enc_text = base64.b64encode(encrypted_msg)
-    return enc_text
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
 
 
 def sign_license(email, expiry, private_key):
     message = f'{email}:{expiry}'
-    return private_key_encrypt(message, private_key)
+    # hash
+    digest = SHA256.new()
+    digest.update(message.encode('utf-8'))
+    # sign
+    signer = PKCS1_v1_5.new(private_key)
+    signature = signer.sign(digest)
+    return signature.hex()
 
 
 def read_private_key(filename):
@@ -26,11 +25,13 @@ def read_private_key(filename):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: python sign.py <private_key_file> <email> <expiry>")
+        print("Usage: python sign.py <private_key_file> <email> <expiry?>")
         sys.exit(1)
     
     key = read_private_key(sys.argv[1])
     email = sys.argv[2]
-    expiry, = sys.argv[3:4] or [ str(int(time.time())) ]
+    expiry, = sys.argv[4:5] or [ str(int((time.time() + 31_536_000) * 1000)) ] # default expiry: 1 year
+
     
     print(sign_license(email, expiry, key))
+    print(email, expiry)
