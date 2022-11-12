f87a1b8fb097f9e345b4f04890787329d9276b4a
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-11-12 01:17:23 +0100

    Fixed verification script

--- verify.py
+++ verify.py
@@ -1,15 +1,20 @@
 #!/usr/local/env python
 
 import sys
-import base64
 import time
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
 
 
-def public_key_decrypt(encrypted_msg, public_key):
-    cipher = PKCS1_OAEP.new(public_key)
-    return cipher.decrypt(base64.b64decode(encrypted_msg))
+def verify_license(email, expiry, license, public_key):
+    message = f'{email}:{expiry}'
+    # hash
+    digest = SHA256.new()
+    digest.update(message.encode('utf-8'))
+    # verify
+    verifier = PKCS1_v1_5.new(public_key)
+    return verifier.verify(digest, bytes.fromhex(license))
 
 
 def read_public_key(filename):
@@ -21,11 +26,6 @@ def read_signature_file(filename):
     with open(filename, "r", encoding='utf-8') as f:
         return f.read()
     
-    
-def verify_license(email, expiry, signature, public_key):
-    message = f'{email}:{expiry}'
-    print(message)
-    return message == public_key_decrypt(signature, public_key).decode('utf-8')
 
 def verify_expiry(expiry):
     return time.time() < float(expiry) / 1000.0
@@ -36,13 +36,13 @@ def verify(email, expiry, signature, public_key):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: python verify.py <public_key_file> <signature_file> <email> <expiry?>")
+        print("Usage: python verify.py <public_key_file> <signature_file> <email> <expiry>")
         sys.exit(1)
     
     key = read_public_key(sys.argv[1])
     signature = read_signature_file(sys.argv[2])[0:-1]
     email = sys.argv[3]
-    expiry, = sys.argv[4:5] or [ str(int((time.time() + 31_536_000) * 1000)) ] # default expiry: 1 year
+    expiry = sys.argv[4]
     
     print(signature, email, expiry)
     
